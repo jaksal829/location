e8616242ca9b50e35eba91b64196e2636c7fee3f
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2023-04-03 13:34:39 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,16 +9,49 @@
 <!-- 지도를 표시할 div 입니다 -->
 <div id="map" style="width:100%;height:350px;"></div>
 
-<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=dce8def3a21921f5d72d060cd4da648a"></script>
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=f6e4b36ec6c88cd63ebbe33a1dda73f2"></script>
 <script>
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        center: new kakao.maps.LatLng(35.1800298, 128.7387107), // 지도의 중심좌표
+        level: 10 // 지도의 확대 레벨
     };
 
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+
+</script>
+
+<script> // 함수 선언
+
+var alat = document.getElementById("demo1");    
+var alon = document.getElementById("demo2");
+
+function getlocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else { 
+    }
+}
+
+function showPosition(position) {
+  alat.innerHTML = position.coords.latitude;
+  alon.innerHTML = position.coords.longitude;
+}
+</script>
+
+<script>
+    var markerPosition  = new kakao.maps.LatLng(alat, alon); 
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: markerPosition
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
 </script>
 </body>
 </html>
