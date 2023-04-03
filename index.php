5045524c7b4c4e2ee933b83f9e2104ae8bca1daf
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2023-04-03 13:38:39 +0900

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- 지도를 표시할 div 입니다 -->
     <div id="map" style="width:100%;height:350px;"></div>
 
-    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=f6e4b36ec6c88cd63ebbe33a1dda73f2"></script>
+    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=dce8def3a21921f5d72d060cd4da648a"></script>
     <script>
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
         mapOption = { 
@@ -20,7 +20,25 @@
     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
     var map = new kakao.maps.Map(mapContainer, mapOption); 
 
-    var markerPosition  = new kakao.maps.LatLng(35.1800298, 128.7387107); 
+
+    var alat = document.getElementById("demo");    
+    var alon = document.getElementById("demo");
+
+    function getlocation(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(showPosition);
+        }
+        else { 
+        }
+    }
+
+    function showPosition(position) {
+    alat.innerHTML = position.coords.latitude;
+    alon.innerHTML = position.coords.longitude;
+    }
+    getlocation();
+
+    var markerPosition  = new kakao.maps.LatLng(alat, alon); 
 
     // 마커를 생성합니다
     var marker = new kakao.maps.Marker({
