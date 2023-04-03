786b37285431194ab26ae9252d447dee27876c03
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2023-04-03 13:40:58 +0900

    Update index.php

--- index.php
+++ index.php
@@ -11,42 +11,27 @@
 
     <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=dce8def3a21921f5d72d060cd4da648a"></script>
     <script>
-    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-        mapOption = { 
-            center: new kakao.maps.LatLng(35.1800298, 128.7387107), // 지도의 중심좌표
-            level: 10 // 지도의 확대 레벨
-        };
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
 
-    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-    var map = new kakao.maps.Map(mapContainer, mapOption); 
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
 
-    var alat = document.getElementById("demo");    
-    var alon = document.getElementById("demo");
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
 
-    function getlocation(){
-        if(navigator.geolocation){
-            navigator.geolocation.getCurrentPosition(showPosition);
-        }
-        else { 
-        }
-    }
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
 
-    function showPosition(position) {
-    alat.innerHTML = position.coords.latitude;
-    alon.innerHTML = position.coords.longitude;
-    }
-    getlocation();
-
-    var markerPosition  = new kakao.maps.LatLng(alat, alon); 
-
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-        position: markerPosition
-    });
-
-    // 마커가 지도 위에 표시되도록 설정합니다
-    marker.setMap(map);
+// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+// marker.setMap(null);    
 </script>
 </body>
 </html>
