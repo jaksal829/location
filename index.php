8557088d59a4dbd1b9d4db0c79bf1fede70ed3aa
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2023-04-05 16:45:29 +0900

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <script>
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 초기 중심좌표
         level: 10 // 지도의 확대 레벨 
     }; 
 
@@ -29,11 +29,11 @@ if (navigator.geolocation) {
         var lat = position.coords.latitude, // 위도
             lon = position.coords.longitude; // 경도
         
-        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-            message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+        var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+            //message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
         
         // 마커와 인포윈도우를 표시합니다
-        displayMarker(locPosition, message);
+        displayMarker(locPosition);
         map.setLevel(level);
             
       });
@@ -59,13 +59,14 @@ function displayMarker(locPosition, message) {
         iwRemoveable = true;
 
     // 인포윈도우를 생성합니다
-    //var infowindow = new kakao.maps.InfoWindow({
-        //content : iwContent,
-        //removable : iwRemoveable
-    //});
+    var infowindow = new kakao.maps.InfoWindow({
+        content : iwContent,
+        removable : iwRemoveable
+    });
     
     // 인포윈도우를 마커위에 표시합니다 
-    //infowindow.open(map, marker);
+    if(navigator.geolocation == false)
+    	infowindow.open(map, marker);
     
     // 지도 중심좌표를 접속위치로 변경합니다
     map.setCenter(locPosition); 
